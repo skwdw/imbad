cddf706d25d46f7983f6102709cb0ed18acd89c1
skwdw <75801773+skwdw@users.noreply.github.com>
2021-07-24 19:56:35 +0900

    Add files via upload

---
+++ embed.py
@@ -0,0 +1,60 @@
+
+###################
+####python 3.x#####
+#discord.py==1.4.0#
+###################
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("봇이 성공적으로 실행되었습니다.")
+    game = discord.Game('★BTARSERVER★')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!색깔"):
+        await message.channel.send('`빨강` `주황` `노랑` `초록` `파랑` `보라` `분홍` `검정` `민트` `하늘` `갈색` `회색` `남색`')
+
+    if message.content.startswith("!임베드"):
+        if message.content[5:7] == '빨강':
+            selcolor = 0xFF0000
+        if message.content[5:7] == '주황':
+            selcolor = 0xFF8C00
+        if message.content[5:7] == '노랑':
+            selcolor = 0xFFDC37
+        if message.content[5:7] == '초록':
+            selcolor = 0x00FC08
+        if message.content[5:7] == '파랑':
+            selcolor = 0x006AFF
+        if message.content[5:7] == '보라':
+            selcolor = 0x9932CC
+        if message.content[5:7] == '분홍':
+            selcolor = 0xFF00FF
+        if message.content[5:7] == '검정':
+            selcolor = 0x000000
+        if message.content[5:7] == '민트':
+            selcolor = 0x00FFDD
+        if message.content[5:7] == '하늘':
+            selcolor = 0x3CFBFF
+        if message.content[5:7] == '갈색':
+            selcolor = 0x8B4F1D
+        if message.content[5:7] == '회색':
+            selcolor = 0x828282
+        if message.content[5:7] == '남색':
+            selcolor = 0x3700FF
+
+        value = message.content[8:]
+        embed = discord.Embed(color=selcolor)
+        embed.add_field(name="\u200b", value=value, inline=False)
+        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+
+        await message.channel.send(embed=embed)
+        await message.delete()
+
+
+client.run('★ODY4Mzc2NzE0MDE3OTE4OTk2.YPuw1g.fnJ1jEQq8hOvlJGKh5klK0ic9uM★')
